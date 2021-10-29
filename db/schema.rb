54013d9c4918506fc09fa325d7c0c0b104ba30e0
John Russell <john.russell@tufts.edu>
2021-10-29 13:05:04 -0400

    Migration: add current_date to Game (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_144936) do
+ActiveRecord::Schema.define(version: 2021_10_29_121346) do
 
   create_table "games", force: :cascade do |t|
     t.date "start_date", null: false
     t.date "end_date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "current_date", null: false
   end
 
 end
