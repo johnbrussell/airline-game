e99bd14d0a891592c23212e8547b30a91491f510
JBR <john.russell@tufts.edu>
2021-10-29 12:58:34 -0400

    Validate that current_date is never less than start_date or greater than end_date

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,20 @@ class Game < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :current_date, presence: true
+  validate :current_date_greater_than_start_date
+  validate :current_date_less_than_end_date
+
+  private
+
+  def current_date_greater_than_start_date
+    if current_date < start_date
+      errors.add(:current_date, "cannot be less than start_date")
+    end
+  end
+
+  def current_date_less_than_end_date
+    if current_date > end_date
+      errors.add(:current_date, "cannot be greater than end_date")
+    end
+  end
 end
---
+++ test/models/game_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class GameTest < ActiveSupport::TestCase
+  today = Date.today
+  yesterday = today - 1
+  tomorrow = today + 1
+
+  test "current_date fails validation when less than start_date" do
+    subject = Game.new(start_date: today, end_date: tomorrow, current_date: yesterday)
+
+    assert_not(subject.save)
+    assert_not(subject.valid?)
+    assert_includes(subject.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be less than start_date")
+  end
+
+  test "current_date fails validation when greater than end_date" do
+    subject = Game.new(start_date: yesterday, end_date: today, current_date: tomorrow)
+
+    assert_not(subject.save)
+    assert_not(subject.valid?)
+    assert_includes(subject.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be greater than end_date")
+  end
+
+  test "current_date fails validation when start_date is greater than end_date" do
+    subject_1 = Game.new(start_date: tomorrow, end_date: yesterday, current_date: today)
+    subject_2 = Game.new(start_date: tomorrow, end_date: yesterday, current_date: yesterday)
+    subject_3 = Game.new(start_date: tomorrow, end_date: yesterday, current_date: tomorrow)
+
+    [subject_1, subject_2, subject_3].each do |subject|
+      assert_not(subject.save)
+      assert_not(subject.valid?)
+    end
+
+    assert_includes(subject_1.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be less than start_date")
+    assert_includes(subject_1.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be greater than end_date")
+    assert_includes(subject_2.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be less than start_date")
+    assert_includes(subject_3.errors.map{ |error| "#{error.attribute} #{error.message}" }, "current_date cannot be greater than end_date")
+  end
+
+  test "current_date does not fail validation when between start_date and end_date" do
+    subject_1 = Game.new(start_date: yesterday, end_date: tomorrow, current_date: yesterday)
+    subject_2 = Game.new(start_date: yesterday, end_date: tomorrow, current_date: today)
+    subject_3 = Game.new(start_date: yesterday, end_date: tomorrow, current_date: tomorrow)
+
+    [subject_1, subject_2, subject_3].each do |subject|
+      assert(subject.valid?)
+      assert(subject.save)
+    end
+  end
+end
