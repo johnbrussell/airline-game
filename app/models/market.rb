1a7265e6b5bbe31a5b83eec4a6f2c61ec54c750c
JBR <john.russell@tufts.edu>
2021-11-03 13:19:15 -0400

    Add validations to Market

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,2 +1,7 @@
 class Market < ApplicationRecord
+  validates :name, presence: true
+  validates :country, presence: true
+  validates :income, presence: true
+  validates :is_national_capital, presence: true
+  validates :is_island, presence: true
 end
