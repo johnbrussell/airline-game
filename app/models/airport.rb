3a28c11d00d4be1931018915839d103fdf51045a
John Russell <john.russell@tufts.edu>
2021-11-11 10:41:42 -0500

    Validate numericality (#22)
    * Validate numericality of Airport fields
    
    * Numericality vaidations on Market
    
    * Validate numericality of Population
    
    * Validate numericality of Tourists

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -3,12 +3,19 @@ class Airport < ApplicationRecord
 
   validates :iata, presence: true
   validates :exclusive_catchment, presence: true
+  validates :exclusive_catchment, numericality: { greater_than_or_equal_to: 0, less_than: 1 }
   validates :runway, presence: true
+  validates :runway, numericality: { greater_than: 0 }
   validates :elevation, presence: true
+  validates :elevation, numericality: { greater_than: -1411, less_than: 29032 }
   validates :start_gates, presence: true
+  validates :start_gates, numericality: { greater_than_or_equal_to: 1 }
   validates :easy_gates, presence: true
+  validates :easy_gates, numericality: { greater_than_or_equal_to: :start_gates }
   validates :latitude, presence: true
+  validates :latitude, numericality: { greater_than: -90, less_than: 90 }
   validates :longitude, presence: true
+  validates :longitude, numericality: { greater_than: -180, less_than: 180 }
 
   validates_uniqueness_of :iata
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -2,8 +2,11 @@ class Market < ApplicationRecord
   validates :name, presence: true
   validates :country, presence: true
   validates :income, presence: true
+  validates :income, numericality: { greater_than: 0 }
   validates :is_national_capital, :inclusion => { :in => [true, false] }
   validates :is_island, :inclusion => { :in => [true, false] }
+  validates :business_demand, numericality: { greater_than_or_equal_to: 0 }
+  validates :leisure_demand, numericality: { greater_than_or_equal_to: 0 }
 
   validates_uniqueness_of :name
 
