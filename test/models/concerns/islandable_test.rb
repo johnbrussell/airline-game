e97c688aafa740094a0b729abfd8842ff363d3db
JBR <john.russell@tufts.edu>
2021-11-11 17:37:52 -0500

    Islandable

---
+++ test/models/concerns/islandable_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class IslandableTest < ActiveSupport::TestCase
+  island_airport = Airport.new
+  island = Market.new(is_island: true, airports: [island_airport])
+
+  mainland_airport = Airport.new
+  mainland = Market.new(is_island: false, airports: [mainland_airport])
+
+  test "island_to_island? returns false if neither origin nor destination is an island" do
+    subject = TestClass.new(mainland_airport, mainland_airport, Date.today)
+
+    assert_not subject.send(:island_to_island?)
+  end
+
+  test "island_to_island? returns false if the origin is not an island" do
+    subject = TestClass.new(mainland_airport, island_airport, Date.today)
+
+    assert_not subject.send(:island_to_island?)
+  end
+
+  test "island_to_island? returns false if the destination is not an island" do
+    subject = TestClass.new(island_airport, mainland_airport, Date.today)
+
+    assert_not subject.send(:island_to_island?)
+  end
+
+  test "island_to_island? returns true if origin and destination are both islands" do
+    subject = TestClass.new(island_airport, island_airport, Date.today)
+
+    assert subject.send(:island_to_island?)
+  end
+end
+
+class TestClass
+  include Islandable
+end
