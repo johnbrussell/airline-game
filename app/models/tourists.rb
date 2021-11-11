9e432f61cbaad6a58edfea129bd4c1b12468d22c
JBR <john.russell@tufts.edu>
2021-11-11 10:40:59 -0500

    Validate numericality of Tourists

--- app/models/tourists.rb
+++ app/models/tourists.rb
@@ -1,4 +1,6 @@
 class Tourists < ApplicationRecord
   validates :volume, presence: true
+  validates :volume, numericality: { greater_than_or_equal_to: 0 }
   validates :year, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1914 }
 end
