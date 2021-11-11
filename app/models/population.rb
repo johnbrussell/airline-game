f0dc0d3f87af381b980b2641c369dfdd991e55c5
JBR <john.russell@tufts.edu>
2021-11-11 10:40:15 -0500

    Validate numericality of Population

--- app/models/population.rb
+++ app/models/population.rb
@@ -1,4 +1,6 @@
 class Population < ApplicationRecord
   validates :population, presence: true
+  validates :population, numericality: { greater_than_or_equal_to: 0 }  # 0 okay because maybe some places are founded after 1914
   validates :year, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1914 }
 end
