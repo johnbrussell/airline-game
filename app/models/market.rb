7f5dfd42607732f3461c91d3dbfcfc50d4f9e1c7
JBR <john.russell@tufts.edu>
2021-11-11 11:07:12 -0500

    Remove demand validations from Market

--- app/models/market.rb
+++ app/models/market.rb
@@ -5,8 +5,6 @@ class Market < ApplicationRecord
   validates :income, numericality: { greater_than: 0 }
   validates :is_national_capital, :inclusion => { :in => [true, false] }
   validates :is_island, :inclusion => { :in => [true, false] }
-  validates :business_demand, numericality: { greater_than_or_equal_to: 0 }
-  validates :leisure_demand, numericality: { greater_than_or_equal_to: 0 }
 
   validates_uniqueness_of :name
 
