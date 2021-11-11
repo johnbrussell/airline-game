495155162f9817bced804d4ffe068fd47efa3487
JBR <john.russell@tufts.edu>
2021-11-11 18:19:24 -0500

    Revert "GlobalDemand.out_of_date_from?"
    This reverts commit 1ee290729662c204905a6f2bfd0412a827932b3f.

--- app/models/global_demand.rb
+++ app/models/global_demand.rb
@@ -1,10 +1,4 @@
 class GlobalDemand < ApplicationRecord
   validates :business, numericality: { greater_than_or_equal_to: 0 }
   validates :leisure, numericality: { greater_than_or_equal_to: 0 }
-
-  OUT_OF_DATE_DISTANCE_DAYS = 182.days
-
-  def out_of_date_from?(test_date)
-    (date - test_date).abs().days > OUT_OF_DATE_DISTANCE_DAYS
-  end
 end
