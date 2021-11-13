49a4748a91ae286c4ada6c2e991e62e25591af55
John Russell <john.russell@tufts.edu>
2021-11-13 14:15:37 -0500

    Add columns to GlobalDemand (#35)
    * Migration: Add tourist and government demand to GlobalDemand
    
    * Validate new columns on GlobalDemand
    
    * Validate presence of columns on GlobalData

--- app/models/global_demand.rb
+++ app/models/global_demand.rb
@@ -1,4 +1,10 @@
 class GlobalDemand < ApplicationRecord
   validates :business, numericality: { greater_than_or_equal_to: 0 }
+  validates :business, presence: true
+  validates :government, numericality: { greater_than_or_equal_to: 0 }
+  validates :government, presence: true
   validates :leisure, numericality: { greater_than_or_equal_to: 0 }
+  validates :leisure, presence: true
+  validates :tourist, numericality: { greater_than_or_equal_to: 0 }
+  validates :tourist, presence: true
 end
