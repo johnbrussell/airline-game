cf46cb1182793a230d4f3b1945fdf1356f694059
John Russell <john.russell@tufts.edu>
2021-11-13 11:48:25 -0500

    BusnessDemand -> ResidentDemand which can calculate leisure demand (#29)

---
+++ app/models/calculation/resident_demand.rb
@@ -0,0 +1,38 @@
+class Calculation::ResidentDemand
+  include Islandable
+  include Populatable
+
+  def business_demand
+    raw_demand(:business)
+  end
+
+  def leisure_demand
+    raw_demand(:leisure)
+  end
+
+  private
+
+    def border_multiplier
+      if origin_market.is_island
+        domestic? ? 1 : 1/12.0
+      else
+        domestic? ? 1 : 1/4.0
+      end
+    end
+
+    def distance_demand(type)
+      if origin_market.is_island
+        Calculation::DemandCurve.new(distance, type).relative_demand_island
+      else
+        Calculation::DemandCurve.new(distance, type).relative_demand
+      end
+    end
+
+    def raw_demand(type)
+      if origin_market == destination_market
+        0
+      else
+        airport_population / 100.0 * distance_demand(type) * border_multiplier * island_multipler
+      end
+    end
+end
