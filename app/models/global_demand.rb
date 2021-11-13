613b80b58fe1d59fd6fb3bed07f04a6040e25d32
JBR <john.russell@tufts.edu>
2021-11-13 14:03:05 -0500

    Validate new columns on GlobalDemand

--- app/models/global_demand.rb
+++ app/models/global_demand.rb
@@ -1,4 +1,6 @@
 class GlobalDemand < ApplicationRecord
   validates :business, numericality: { greater_than_or_equal_to: 0 }
+  validates :government, numericality: { greater_than_or_equal_to: 0 }
   validates :leisure, numericality: { greater_than_or_equal_to: 0 }
+  validates :tourist, numericality: { greater_than_or_equal_to: 0 }
 end
