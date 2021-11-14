24dbd91f41d44d2b373289df4414761b4c789d8e
JBR <john.russell@tufts.edu>
2021-11-13 19:08:59 -0500

    Calculation::TotalMarketDemand

---
+++ app/models/calculation/total_market_demand.rb
@@ -0,0 +1,103 @@
+class Calculation::TotalMarketDemand
+  def self.business(origin_airport, destination_market, date)
+    self.total_demand(
+      self.destination_business_demands(
+        origin_airport,
+        destination_market,
+        date,
+      ),
+      destination_market,
+    )
+  end
+
+  def self.government(origin_airport, destination_market, date)
+    self.total_demand(
+      self.destination_government_demands(
+        origin_airport,
+        destination_market,
+        date,
+      ),
+      destination_market,
+    )
+  end
+
+  def self.leisure(origin_airport, destination_market, date)
+    self.total_demand(
+      self.destination_leisure_demands(
+        origin_airport,
+        destination_market,
+        date,
+      ),
+      destination_market,
+    )
+  end
+
+  def self.tourist(origin_airport, destination_market, date)
+    self.total_demand(
+      self.destination_tourist_demands(
+        origin_airport,
+        destination_market,
+        date,
+      ),
+      destination_market,
+    )
+  end
+
+  private
+
+    def self.destination_business_demands(origin_airport, destination_market, date)
+      destination_market.airports.map { |airport|
+        {
+          airport: airport,
+          demand: Calculation::ResidentDemand.new(origin_airport, airport, date).business_demand,
+        }
+      }
+    end
+
+    def self.destination_government_demands(origin_airport, destination_market, date)
+      destination_market.airports.map { |airport|
+        {
+          airport: airport,
+          demand: Calculation::GovernmentDemand.new(origin_airport, airport, date).demand,
+        }
+      }
+    end
+
+    def self.destination_leisure_demands(origin_airport, destination_market, date)
+      destination_market.airports.map { |airport|
+        {
+          airport: airport,
+          demand: Calculation::ResidentDemand.new(origin_airport, airport, date).leisure_demand,
+        }
+      }
+    end
+
+    def self.destination_tourist_demands(origin_airport, destination_market, date)
+      destination_market.airports.map { |airport|
+        {
+          airport: airport,
+          demand: Calculation::TouristDemand.new(origin_airport, airport, date).demand,
+        }
+      }
+    end
+
+    def self.exclusive_demands(destination_demands, destination_market)
+      destination_demands.map { |demand|
+        demand.fetch(:demand) * demand.fetch(:airport).exclusive_catchment.to_f / (destination_market.shared_catchment + demand.fetch(:airport).exclusive_catchment)
+      }
+    end
+
+    def self.shared_demand(destination_demands, market)
+      self.shared_demands(destination_demands, market).max
+    end
+
+    def self.shared_demands(destination_demands, market)
+      destination_demands.map { |demand|
+        demand.fetch(:demand) * market.shared_catchment.to_f / (market.shared_catchment + demand.fetch(:airport).exclusive_catchment)
+      }
+    end
+
+    def self.total_demand(individual_demands, destination_market)
+      self.shared_demand(individual_demands, destination_market) + self.exclusive_demands(individual_demands, destination_market).sum
+    end
+end
