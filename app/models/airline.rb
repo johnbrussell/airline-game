d319b223949af12095e10b87616d50088d686b05
JBR <john.russell@tufts.edu>
2021-11-19 16:32:52 -0500

    Validate cash on hand

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,5 @@
 class Airline < ApplicationRecord
+  validates :cash_on_hand, presence: true
   validates :name, presence: true
   validates :is_user_airline, :inclusion => { :in => [true, false] }
 end
