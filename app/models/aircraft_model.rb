b1ab890592cb38a82d3890bdacb862adee05f3f5
JBR <john.russell@tufts.edu>
2021-11-19 15:52:21 -0500

    AircraftModel model

---
+++ app/models/aircraft_model.rb
@@ -0,0 +1,25 @@
+class AircraftModel < ApplicationRecord
+  validates :manufacturer, presence: true
+  validates :family, presence: true
+  validates :name, presence: true
+  validates :production_start_year, presence: true
+  validates :production_start_year, numericality: { greater_than_or_equal_to: 1914 }
+  validates :floor_space, presence: true
+  validates :floor_space, numericality: { greater_than: 0 }
+  validates :max_range, presence: true
+  validates :max_range, numericality: { greater_than: 0 }
+  validates :fuel_burn, presence: true
+  validates :fuel_burn, numericality: { greater_than: 0 }
+  validates :speed, presence: true
+  validates :speed, numericality: { greater_than: 0 }
+  validates :num_pilots, presence: true
+  validates :num_pilots, numericality: { greater_than: 0 }
+  validates :num_flight_attendants, presence: true
+  validates :num_flight_attendants, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :takeoff_distance, presence: true
+  validates :takeoff_distance, numericality: { greater_than: 0 }
+  validates :useful_life, presence: true
+  validates :useful_life, numericality: { greater_than: 0 }
+end
