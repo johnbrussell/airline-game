77c07c5e925602d4a97e58b1f2a18ac8a6d767c9
JBR <john.russell@tufts.edu>
2021-11-19 23:18:32 -0500

    Add cash_on_hand to airlines in view tests

--- test/views/games/index_test.rb
+++ test/views/games/index_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
 class IndexTest < ActionDispatch::IntegrationTest
-  airline_1 = Airline.new(name: "1")
-  airline_2 = Airline.new(name: "2")
-  airline_3 = Airline.new(name: "3")
+  airline_1 = Airline.new(name: "1", cash_on_hand: 1234.56)
+  airline_2 = Airline.new(name: "2", cash_on_hand: 1234.56)
+  airline_3 = Airline.new(name: "3", cash_on_hand: 1234.56)
   today = Date.today
   tomorrow = today + 1
   game_1 = Game.new(start_date: today, end_date: tomorrow, current_date: today, airlines: [airline_1])
 
   test "the page singularizes correctly when there is only one game" do
-    game_1.save
+    game_1.save!
 
     get "/"
     assert_response(:success)
@@ -19,8 +19,8 @@ class IndexTest < ActionDispatch::IntegrationTest
   end
 
   test "all games are listed on the page" do
-    game_1.save
-    Game.new(start_date: tomorrow, end_date: tomorrow, current_date: tomorrow, airlines: [airline_2, airline_3]).save
+    game_1.save!
+    Game.new(start_date: tomorrow, end_date: tomorrow, current_date: tomorrow, airlines: [airline_2, airline_3]).save!
 
     get "/"
     assert_response(:success)
