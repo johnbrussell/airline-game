a2114b6928c0469df783fe6ac288e44b8c74b2aa
JBR <john.russell@tufts.edu>
2021-11-20 17:25:52 -0500

    Calculation::SlotRent

---
+++ app/models/calculation/slot_rent.rb
@@ -0,0 +1,20 @@
+class Calculation::SlotRent
+  SCALE_CONSTANT = 0.00544331054
+
+  def self.calculate(airport, game)
+    gates = Gates.at_airport(airport, game)
+    SCALE_CONSTANT * (dollars_in_market(gates) ** 0.5) * (gates.current_gates ** 0.5) * (gates_in_market(gates) ** 0.5)
+  end
+
+  private
+
+    def self.dollars_in_market(gates)
+      market_dollar_calculator = Calculation::MarketDollars.new(gates.airport, gates.game.current_date)
+      market_dollar_calculator.business + market_dollar_calculator.leisure
+    end
+
+    def self.gates_in_market(gates)
+      market = gates.airport.market
+      market.airports.flat_map { |airport| Gates.at_airport(airport, gates.game) }.sum(&:current_gates)
+    end
+end
---
+++ spec/models/calculation/slot_rent_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe Calculation::SlotRent do
+  context "calculate" do
+    it "calculates correctly" do
+      market_mock = instance_double(Market)
+      airport_mock = instance_double(Airport, market: market_mock)
+      game_mock = instance_double(Game, current_date: Date.today)
+      gates_mock = instance_double(Gates, airport: airport_mock, game: game_mock, current_gates: 9)
+      market_dollars_mock = instance_double(Calculation::MarketDollars, business: 1000, leisure: 9000)
+
+      expect(Gates).to receive(:at_airport).twice.with(airport_mock, game_mock).and_return(gates_mock)
+      expect(Calculation::MarketDollars).to receive(:new).with(airport_mock, Date.today).and_return(market_dollars_mock)
+      expect(market_mock).to receive(:airports).and_return([airport_mock])
+
+      expect(described_class.calculate(airport_mock, game_mock)).to eq 900 * Calculation::SlotRent::SCALE_CONSTANT
+    end
+  end
+end
