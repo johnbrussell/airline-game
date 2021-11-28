8311f13c408ca94e266011f550b3b1a4aa4ad392
John Russell <john.russell@tufts.edu>
2021-11-28 12:40:52 -0500

    Aircraft purchase prices (#60)
    * Aircraft.is_owned? -> .has_operator?
    
    * Migration: add ownership and lease information to Airplane
    
    * Airplanes have purchase prices based on age

--- app/models/aircraft_model.rb
+++ app/models/aircraft_model.rb
@@ -22,4 +22,11 @@ class AircraftModel < ApplicationRecord
   validates :useful_life, numericality: { greater_than: 0 }
 
   belongs_to :family, class_name: "AircraftFamily", foreign_key: "aircraft_family_id"
+
+  DAYS_PER_YEAR = 365.24
+  PERCENT_VALUE_MAINTAINED_AT_END_OF_USEFUL_LIFE = 0.03
+
+  def daily_value_retention
+    PERCENT_VALUE_MAINTAINED_AT_END_OF_USEFUL_LIFE ** (1 / (DAYS_PER_YEAR * useful_life))
+  end
 end
