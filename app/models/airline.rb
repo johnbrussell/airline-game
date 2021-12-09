3e3e46f29e649e211bca2ff98ffef3869c34ea8f
JBR <john.russell@tufts.edu>
2021-12-08 20:48:54 -0500

    don't destroy user airlines

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -4,11 +4,20 @@ class Airline < ApplicationRecord
   validates :is_user_airline, :inclusion => { :in => [true, false] }
   validate :only_one_user_airline_exists
 
+  before_destroy :validate_a_user_airline_exists
+
   private
 
     def only_one_user_airline_exists
       if is_user_airline && Airline.where(is_user_airline: true).count > 0
         errors.add(:is_user_airline, "cannot be true for multiple airlines")
+      end
+    end
+
+    def validate_a_user_airline_exists
+      if is_user_airline
+        errors.add(:is_user_airline, "must be true for at least one airline at all times")
+        throw :abort
       end
     end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -33,4 +33,26 @@ RSpec.describe Airline do
       expect(new_airline.errors.map{ |error| "#{error.attribute} #{error.message}" }).to include ("is_user_airline cannot be true for multiple airlines")
     end
   end
+
+  context "validate_a_user_airline_exists" do
+    it "is false for a user airline" do
+      airline = Airline.create!(cash_on_hand: 1, name: "foo", is_user_airline: true)
+
+      expect(Airline.count).to eq 1
+      airline.destroy
+      expect(Airline.count).to eq 1
+      expect(airline.errors.map{ |error| "#{error.attribute} #{error.message}" }).to include "is_user_airline must be true for at least one airline at all times"
+    end
+
+    it "is true for a non-user airline" do
+      airline_1 = Airline.create!(cash_on_hand: 1, name: "foo", is_user_airline: false)
+      airline_2 = Airline.create!(cash_on_hand: 1, name: "bar", is_user_airline: true)
+
+      expect(Airline.count).to eq 2
+      airline_1.destroy
+      expect(Airline.count).to eq 1
+
+      expect(Airline.last.id).to eq airline_2.id
+    end
+  end
 end
