6a03d412f434e2886ffd39b03560cf1b2e894412
JBR <john.russell@tufts.edu>
2021-12-26 15:30:47 -0500

    Validate presence of base_id on Airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,5 @@
 class Airline < ApplicationRecord
+  validates :base_id, presence: true
   validates :cash_on_hand, presence: true
   validates :name, presence: true
   validates :is_user_airline, :inclusion => { :in => [true, false] }
