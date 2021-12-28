678fd82f944a5a2638b39698d3d05b40ab0fdca4
JBR <john.russell@tufts.edu>
2021-12-28 15:41:40 -0500

    Display used and new aircraft available for purchase or lease

--- spec/views/new_airplanes/airplanes/index.html.erb_spec.rb
+++ spec/views/new_airplanes/airplanes/index.html.erb_spec.rb
@@ -3,18 +3,23 @@ require "capybara/rspec"
 
 RSpec.describe "new_airplanes/airplanes/index", type: :feature do
   context "index" do
-    it "shows the number of new aircraft and number of used aircraft available" do
+    it "shows the number of new aircraft available" do
       game = Game.create!(start_date: Date.yesterday, current_date: Date.today, end_date: Date.tomorrow)
 
-      new_airplane = instance_double(Airplane)
+      unsorted_aircraft = double
+      family = instance_double(AircraftFamily, manufacturer: "Z")
+      model = instance_double(AircraftModel, family: family, name: "200")
+      new_airplane = instance_double(Airplane, aircraft_model: model, construction_date: "1915-01-01".to_date, economy_seats: 10, premium_economy_seats: 9, business_seats: 8)
       new_aircraft = [new_airplane]
-      expect(Airplane).to receive(:available_new).and_return(new_aircraft)
+      expect(Airplane).to receive(:available_new).and_return(unsorted_aircraft)
+      allow(unsorted_aircraft).to receive(:neatly_sorted).and_return(new_aircraft)
 
       visit game_new_airplanes_airplanes_path(game)
 
       expect(page).to have_content "There is 1 new airplane available to buy or lease"
       expect(page).to have_link "Return to game overview"
       expect(page).to have_link "View used airplanes for purchase or lease"
+      expect(page).to have_content "Z 200 constructed 1915-01-01. 10 economy, 9 premium economy, 8 business."
     end
   end
 end
--- spec/views/used_airplanes/airplanes/index.html.erb_spec.rb
+++ spec/views/used_airplanes/airplanes/index.html.erb_spec.rb
@@ -3,19 +3,27 @@ require "capybara/rspec"
 
 RSpec.describe "used_airplanes/airplanes/index", type: :feature do
   context "index" do
-    it "shows the number of new aircraft and number of used aircraft available" do
+    it "shows the number of used aircraft available" do
       game = Game.create!(start_date: Date.yesterday, current_date: Date.today, end_date: Date.tomorrow)
 
-      used_airplane_1 = instance_double(Airplane)
-      used_airplane_2 = instance_double(Airplane)
+      unsorted_aircraft = double
+      family_1 = instance_double(AircraftFamily, manufacturer: "Z")
+      model_1 = instance_double(AircraftModel, family: family_1, name: "200")
+      family_2 = instance_double(AircraftFamily, manufacturer: "J")
+      model_2 = instance_double(AircraftModel, family: family_2, name: "3000")
+      used_airplane_1 = instance_double(Airplane, aircraft_model: model_1, construction_date: "2015-01-01".to_date, economy_seats: 10, premium_economy_seats: 9, business_seats: 8)
+      used_airplane_2 = instance_double(Airplane, aircraft_model: model_2, construction_date: "2010-01-01".to_date, economy_seats: 100, premium_economy_seats: 90, business_seats: 80)
       used_aircraft = [used_airplane_1, used_airplane_2]
-      expect(Airplane).to receive(:available_used).and_return(used_aircraft)
+      expect(Airplane).to receive(:available_used).and_return(unsorted_aircraft)
+      allow(unsorted_aircraft).to receive(:neatly_sorted).and_return(used_aircraft)
 
       visit game_used_airplanes_airplanes_path(game)
 
       expect(page).to have_content "There are 2 used airplanes available to buy or lease"
       expect(page).to have_link "Return to game overview"
       expect(page).to have_link "View new airplanes for purchase or lease"
+      expect(page).to have_content "J 3000 constructed 2010-01-01. 100 economy, 90 premium economy, 80 business."
+      expect(page).to have_content "Z 200 constructed 2015-01-01. 10 economy, 9 premium economy, 8 business."
     end
   end
 end
