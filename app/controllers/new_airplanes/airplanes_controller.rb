106bfb8f4227086f3451be7dd7c296d8c560e2db
JBR <john.russell@tufts.edu>
2021-12-28 14:35:03 -0500

    Remove unnecessary vars from new and used airplane controllers

--- app/controllers/new_airplanes/airplanes_controller.rb
+++ app/controllers/new_airplanes/airplanes_controller.rb
@@ -2,6 +2,5 @@ class NewAirplanes::AirplanesController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
     @new_airplanes = Airplane.all_available_new_airplanes(@game)
-    @used_airplanes = Airplane.all_available_used_airplanes(@game)
   end
 end
--- app/controllers/used_airplanes/airplanes_controller.rb
+++ app/controllers/used_airplanes/airplanes_controller.rb
@@ -1,7 +1,6 @@
 class UsedAirplanes::AirplanesController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
-    @new_airplanes = Airplane.all_available_new_airplanes(@game)
     @used_airplanes = Airplane.all_available_used_airplanes(@game)
   end
 end
