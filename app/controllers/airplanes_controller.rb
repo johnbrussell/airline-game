9384d856ac807708b44e1c56ed78eae3cdad4e3a
JBR <john.russell@tufts.edu>
2021-12-28 15:04:16 -0500

    Sort airplanes on airline fleet page

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -2,6 +2,10 @@ class AirplanesController < ApplicationController
   def index
     @game = Game.find(params[:game_id])
     @airline = Airline.find(params[:airline_id])
-    @airplanes = Airplane.where(operator_id: @airline.id).where("construction_date <= ?", @game.current_date)
+    @airplanes = Airplane.
+      joins(aircraft_model: :family).
+      where(operator_id: @airline.id).
+      where("construction_date <= ?", @game.current_date).
+      order("aircraft_families.manufacturer", "aircraft_families.name", "aircraft_models.name", "construction_date DESC")
   end
 end
