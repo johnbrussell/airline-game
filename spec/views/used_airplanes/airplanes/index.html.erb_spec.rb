a03ef30de8d73cd5bba6cc832d3b63cc800b404e
JBR <john.russell@tufts.edu>
2021-12-28 13:44:38 -0500

    Used airplanes view

---
+++ spec/views/used_airplanes/airplanes/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+require "capybara/rspec"
+
+RSpec.describe "used_airplanes/airplanes/index", type: :feature do
+  context "index" do
+    it "shows the number of new aircraft and number of used aircraft available" do
+      game = Game.create!(start_date: Date.yesterday, current_date: Date.today, end_date: Date.tomorrow)
+
+      new_airplane = instance_double(Airplane)
+      used_airplane_1 = instance_double(Airplane)
+      used_airplane_2 = instance_double(Airplane)
+      new_aircraft = [new_airplane]
+      used_aircraft = [used_airplane_1, used_airplane_2]
+      expect(Airplane).to receive(:all_available_new_airplanes).and_return(new_aircraft)
+      expect(Airplane).to receive(:all_available_used_airplanes).and_return(used_aircraft)
+
+      visit game_new_airplanes_airplanes_path(game)
+
+      expect(page).to have_content "There is 1 new airplane available to buy or lease"
+      expect(page).to have_content "There are 2 used airplanes available to buy or lease"
+    end
+  end
+end
