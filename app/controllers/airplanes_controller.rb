d3f78e262c32ee0827d83cf67b4b5759b4615992
JBR <john.russell@tufts.edu>
2021-12-31 12:10:46 -0500

    Add route and initial view/controller action for airplane purchase/lease

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -7,4 +7,16 @@ class AirplanesController < ApplicationController
       where("construction_date <= ?", @game.current_date).
       neatly_sorted
   end
+
+  def lease
+    @game = Game.find(params[:game_id])
+    @airplane = Airplane.find(params[:airline_id])
+    @airline = Airline.find(params[:airline_id])
+  end
+
+  def purchase
+    @game = Game.find(params[:game_id])
+    @airplane = Airplane.find(params[:airline_id])
+    @airline = Airline.find(params[:airline_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
     namespace :used_airplanes do
       resources :airplanes
     end
+
+    get "/purchase_airplane", to: "airplanes#purchase"
+    get "/lease_airplane", to: "airplanes#lease"
   end
 end
