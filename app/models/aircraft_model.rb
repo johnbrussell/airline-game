20c21368bea5f8fa38933a80bb120c4a24079703
JBR <john.russell@tufts.edu>
2022-01-01 16:44:11 -0500

    AircraftModel.max_business_seats, max_premium_economy_seats, max_economy_seats

--- app/models/aircraft_model.rb
+++ app/models/aircraft_model.rb
@@ -29,4 +29,16 @@ class AircraftModel < ApplicationRecord
   def daily_value_retention
     PERCENT_VALUE_MAINTAINED_AT_END_OF_USEFUL_LIFE ** (1 / (DAYS_PER_YEAR * useful_life))
   end
+
+  def max_business_seats
+    floor_space / Airplane::BUSINESS_SEAT_SIZE
+  end
+
+  def max_economy_seats
+    floor_space / Airplane::ECONOMY_SEAT_SIZE
+  end
+
+  def max_premium_economy_seats
+    floor_space / Airplane::PREMIUM_ECONOMY_SEAT_SIZE
+  end
 end
