3619238ce2646f509849eba8a14b1e3dc9d81c20
JBR <john.russell@tufts.edu>
2022-01-17 22:03:33 -0500

    fix broken spec

--- spec/views/airports/index.html.erb_spec.rb
+++ spec/views/airports/index.html.erb_spec.rb
@@ -35,8 +35,12 @@ RSpec.describe "airports/index", type: :feature do
   end
 
   it "has a dropdown of airports and redirects to the selected airport" do
-    Airport.create!(iata: "BOS", market: Market.last, runway: 10000, elevation: 1, start_gates: 1, easy_gates: 100, latitude: 1, longitude: 1)
-    Airport.create!(iata: "INU", market: Market.last, runway: 10000, elevation: 1, start_gates: 1, easy_gates: 100, latitude: 1, longitude: 1)
+    boston = Airport.create!(iata: "BOS", market: Market.last, runway: 10000, elevation: 1, start_gates: 1, easy_gates: 100, latitude: 1, longitude: 1)
+    nauru = Airport.create!(iata: "INU", market: Market.last, runway: 10000, elevation: 1, start_gates: 1, easy_gates: 100, latitude: 1, longitude: 1)
+    Population.create!(population: 1000000, year: 2000, market_id: boston.id)
+    Population.create!(population: 100000, year: 2000, market_id: nauru.id)
+    Tourists.create!(volume: 100000, year: 2000, market_id: boston.id)
+    Tourists.create!(volume: 1000, year: 2000, market_id: nauru.id)
 
     visit game_airports_path(Game.last)
 
