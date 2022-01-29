da8d43add0cdbedee3a5660a39fe5dbe558ccd3e
JBR <john.russell@tufts.edu>
2022-01-29 16:38:07 -0500

    Rails validations on AirplaneRoute

--- app/models/airplane_route.rb
+++ app/models/airplane_route.rb
@@ -1,2 +1,10 @@
 class AirplaneRoute < ApplicationRecord
+  validates :airline_route_id, presence: true
+  validates :airplane_id, presence: true
+  validates :block_time_mins, presence: true
+  validates :block_time_mins, numericality: { greater_than_or_equal_to: 0 }
+  validates :flight_cost, presence: true
+  validates :flight_cost, numericality: { greater_than_or_equal_to: 0 }
+  validates :frequencies, presence: true
+  validates :frequencies, numericality: { greater_than_or_equal_to: 0 }
 end
