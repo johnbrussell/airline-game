6d06daad6b565de3af2d57ac50442c7645ae1cef
JBR <john.russell@tufts.edu>
2022-01-29 17:20:02 -0500

    Initial AirlineRoute model

---
+++ app/models/airline_route.rb
@@ -0,0 +1,10 @@
+class AirlineRoute < ApplicationRecord
+  validates :economy_price, presence: true
+  validates :economy_price, numericality: { greater_than: 0 }
+  validates :premium_economy_price, presence: true
+  validates :premium_economy_price, numericality: { greater_than: 0 }
+  validates :business_price, presence: true
+  validates :business_price, numericality: { greater_than: 0 }
+  validates :origin_airport_id, presence: true
+  validates :destination_airport_id, presence: true
+end
