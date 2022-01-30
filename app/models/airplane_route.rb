ac8ccbdc815e8ea66818fdb4e05085f5bfff86aa
JBR <john.russell@tufts.edu>
2022-01-29 22:11:37 -0500

    Update AirplaneRoute validations

--- app/models/airplane_route.rb
+++ app/models/airplane_route.rb
@@ -2,11 +2,11 @@ class AirplaneRoute < ApplicationRecord
   validates :airline_route_id, presence: true
   validates :airplane_id, presence: true
   validates :block_time_mins, presence: true
-  validates :block_time_mins, numericality: { greater_than_or_equal_to: 0 }
+  validates :block_time_mins, numericality: { greater_than: 0 }
   validates :flight_cost, presence: true
-  validates :flight_cost, numericality: { greater_than_or_equal_to: 0 }
+  validates :flight_cost, numericality: { greater_than: 0 }
   validates :frequencies, presence: true
-  validates :frequencies, numericality: { greater_than_or_equal_to: 0 }
+  validates :frequencies, numericality: { greater_than: 0 }
 
   belongs_to :airplane
   belongs_to :route, class_name: "AirlineRoute", foreign_key: :airline_route_id
