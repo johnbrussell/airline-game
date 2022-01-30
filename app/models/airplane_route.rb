399ba9147b0d5aa94431ecc2009a94567ac0444d
JBR <john.russell@tufts.edu>
2022-01-30 15:37:00 -0500

    AirplaneRoute.validate_remaining_routes_connected

--- app/models/airplane_route.rb
+++ app/models/airplane_route.rb
@@ -10,6 +10,8 @@ class AirplaneRoute < ApplicationRecord
 
   validate :routes_connected
 
+  before_destroy :validate_remaining_routes_connected
+
   belongs_to :airplane
   belongs_to :route, class_name: "AirlineRoute", foreign_key: :airline_route_id
 
@@ -18,6 +20,13 @@ class AirplaneRoute < ApplicationRecord
     def routes_connected
       if !airplane.routes_connected_with?(route.origin_airport_iata, route.destination_airport_iata)
         errors.add(:route, "does not connect to airplane's route network")
+      end
+    end
+
+    def validate_remaining_routes_connected
+      if !airplane.routes_connected_without?(route.origin_airport_iata, route.destination_airport_iata)
+        errors.add(:route, "is necessary to keep airplane's routes connected")
+        throw :abort
       end
     end
 end
--- spec/models/airplane_route_spec.rb
+++ spec/models/airplane_route_spec.rb
@@ -110,4 +110,134 @@ RSpec.describe AirplaneRoute do
       expect(subject.errors.full_messages).to include "Route does not connect to airplane's route network"
     end
   end
+
+  context "validate_remaining_routes_connected" do
+    it "is true if the airplane has no other routes" do
+      family = Fabricate(:aircraft_family)
+      airplane = Fabricate(:airplane, aircraft_family: family)
+      inu = Fabricate(:airport, iata: "INU")
+      fun = Fabricate(:airport, iata: "FUN", market: inu.market)
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: fun,
+        destination_airport: inu,
+        distance: 1,
+      )
+      subject = AirplaneRoute.create(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+
+      expect(AirplaneRoute.count).to eq 1
+      subject.destroy
+      expect(AirplaneRoute.count).to eq 0
+    end
+
+    it "is true if the airplane's network remains intact" do
+      family = Fabricate(:aircraft_family)
+      airplane = Fabricate(:airplane, aircraft_family: family)
+      inu = Fabricate(:airport, iata: "INU")
+      fun = Fabricate(:airport, iata: "FUN", market: inu.market)
+      trw = Fabricate(:airport, iata: "TRW", market: inu.market)
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: fun,
+        destination_airport: inu,
+        distance: 1,
+      )
+      AirplaneRoute.create!(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: fun,
+        destination_airport: trw,
+        distance: 1,
+      )
+      subject = AirplaneRoute.create(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+
+      expect(AirplaneRoute.count).to eq 2
+      subject.destroy
+      expect(AirplaneRoute.count).to eq 1
+    end
+
+    it "is false if the airplane's network would be split" do
+      family = Fabricate(:aircraft_family)
+      airplane = Fabricate(:airplane, aircraft_family: family)
+      inu = Fabricate(:airport, iata: "INU")
+      fun = Fabricate(:airport, iata: "FUN", market: inu.market)
+      trw = Fabricate(:airport, iata: "TRW", market: inu.market)
+      maj = Fabricate(:airport, iata: "MAJ", market: inu.market)
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: fun,
+        destination_airport: inu,
+        distance: 1,
+      )
+      subject = AirplaneRoute.create(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: fun,
+        destination_airport: trw,
+        distance: 1,
+      )
+      AirplaneRoute.create!(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+      route = AirlineRoute.create!(
+        economy_price: 1,
+        business_price: 2,
+        premium_economy_price: 1.5,
+        origin_airport: inu,
+        destination_airport: maj,
+        distance: 1,
+      )
+      AirplaneRoute.create!(
+        block_time_mins: 1,
+        frequencies: 1,
+        flight_cost: 1,
+        airplane: airplane,
+        route: route,
+      )
+      subject.reload
+
+      expect(AirplaneRoute.count).to eq 3
+      subject.destroy
+      expect(AirplaneRoute.count).to eq 3
+      expect(subject.errors.full_messages).to include "Route is necessary to keep airplane's routes connected"
+    end
+  end
 end
