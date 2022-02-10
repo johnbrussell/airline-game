3aa1546eab2860817a2ddf21068eb7b054a3d17d
JBR <john.russell@tufts.edu>
2022-02-10 12:07:43 -0500

    AirlineRoute.operators_of_route

--- spec/models/airline_route_spec.rb
+++ spec/models/airline_route_spec.rb
@@ -1,6 +1,41 @@
 require "rails_helper"
 
 RSpec.describe AirlineRoute do
+  context "operators_of_route" do
+    it "includes only routes from airlines that operate the route and sorts them" do
+      inu = Fabricate(:airport, iata: "INU")
+      fun = Fabricate(:airport, iata: "FUN", market: inu.market)
+
+      airline_c = Fabricate(:airline, base_id: inu.market.id, name: "C")
+      airline_a = Fabricate(:airline, base_id: inu.market.id, name: "A")
+      airline_b = Fabricate(:airline, base_id: inu.market.id, name: "B")
+      other_airline = Fabricate(:airline, base_id: inu.market.id)
+      family = Fabricate(:aircraft_family)
+      super_model = Fabricate(:aircraft_model, takeoff_distance: 100, max_range: 13000)
+      airplane_c = Fabricate(:airplane, aircraft_family: family, operator_id: airline_c.id, base_country_group: airline_c.base.country_group, aircraft_model: super_model)
+      airplane_a = Fabricate(:airplane, aircraft_family: family, operator_id: airline_a.id, base_country_group: airline_a.base.country_group, aircraft_model: super_model)
+      airplane_b = Fabricate(:airplane, aircraft_family: family, operator_id: airline_b.id, base_country_group: airline_b.base.country_group, aircraft_model: super_model)
+
+      airline_route_c = AirlineRoute.create!(origin_airport_id: fun.id, destination_airport_id: inu.id, economy_price: 1, premium_economy_price: 2, business_price: 3, distance: 4, airline: airline_c)
+      airline_route_a = AirlineRoute.create!(origin_airport_id: fun.id, destination_airport_id: inu.id, economy_price: 1, premium_economy_price: 2, business_price: 3, distance: 4, airline: airline_a)
+      airline_route_b = AirlineRoute.create!(origin_airport_id: fun.id, destination_airport_id: inu.id, economy_price: 1, premium_economy_price: 2, business_price: 3, distance: 4, airline: airline_b)
+      airplane_route_c = AirplaneRoute.new(airplane: airplane_c, route: airline_route_c, block_time_mins: 1, frequencies: 1, flight_cost: 1).save(validate: false)
+      airplane_route_a = AirplaneRoute.new(airplane: airplane_a, route: airline_route_a, block_time_mins: 1, frequencies: 1, flight_cost: 1).save(validate: false)
+      airplane_route_b = AirplaneRoute.new(airplane: airplane_b, route: airline_route_b, block_time_mins: 1, frequencies: 1, flight_cost: 1).save(validate: false)
+
+      AirlineRoute.create!(origin_airport_id: fun.id, destination_airport_id: inu.id, economy_price: 1, premium_economy_price: 2, business_price: 3, distance: 4, airline: other_airline)
+
+      expect(AirlineRoute.operators_of_route(fun, inu)).to eq [airline_route_a, airline_route_b, airline_route_c]
+    end
+
+    it "is empty if no airline operates the route" do
+      inu = Fabricate(:airport, iata: "INU")
+      fun = Fabricate(:airport, iata: "FUN", market: inu.market)
+
+      expect(AirlineRoute.operators_of_route(fun, inu)).to eq []
+    end
+  end
+
   context "airplanes_available_to_add_service" do
     it "calculates correctly" do
       inu = Fabricate(:airport, iata: "INU")
