a988a3216d49a0882199b78671ea51c300c71ca8
JBR <john.russell@tufts.edu>
2022-02-13 13:08:22 -0500

    IslandException.excepted?

--- app/models/island_exception.rb
+++ app/models/island_exception.rb
@@ -1,4 +1,9 @@
 class IslandException < ApplicationRecord
   validates :market_one, presence: true
   validates :market_two, presence: true
+
+  def self.excepted?(market_1, market_2)
+    find_by(market_one: market_1.name, market_two: market_2.name).present? ||
+      find_by(market_one: market_2.name, market_two: market_1.name).present?
+  end
 end
