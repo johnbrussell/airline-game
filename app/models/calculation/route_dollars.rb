486eae75e607abd754fb821d73f18772e01370ab
JBR <john.russell@tufts.edu>
2022-02-15 17:50:03 -0500

    Memoize origin_route_demand and destination_route_demand in RouteDollars

--- app/models/calculation/route_dollars.rb
+++ app/models/calculation/route_dollars.rb
@@ -6,56 +6,56 @@ class Calculation::RouteDollars
   end
 
   def business
-    directional_business_dollars(origin_global_demand, @destination_airport) +
-      directional_business_dollars(destination_global_demand, @origin_airport)
+    directional_business_dollars(origin_global_demand, destination_route_demand) +
+      directional_business_dollars(destination_global_demand, origin_route_demand)
   end
 
   def government
-    directional_government_dollars(origin_global_demand, @destination_airport) +
-      directional_government_dollars(destination_global_demand, @origin_airport)
+    directional_government_dollars(origin_global_demand, destination_route_demand) +
+      directional_government_dollars(destination_global_demand, origin_route_demand)
   end
 
   def leisure
-    directional_leisure_dollars(origin_global_demand, @destination_airport) +
-      directional_leisure_dollars(destination_global_demand, @origin_airport)
+    directional_leisure_dollars(origin_global_demand, destination_route_demand) +
+      directional_leisure_dollars(destination_global_demand, origin_route_demand)
   end
 
   def tourist
-    directional_tourist_dollars(origin_global_demand, @destination_airport) +
-      directional_tourist_dollars(destination_global_demand, @origin_airport)
+    directional_tourist_dollars(origin_global_demand, destination_route_demand) +
+      directional_tourist_dollars(destination_global_demand, origin_route_demand)
   end
 
   private
 
-    def directional_business_dollars(global_demand, destination)
+    def directional_business_dollars(global_demand, route_demand)
       if global_demand.business == 0
         0
       else
-        RouteDemand.calculate(@date, global_demand.airport, destination).business.to_f / global_demand.business * Calculation::MarketDollars.new(global_demand.airport, @date).business
+        route_demand.business.to_f / global_demand.business * Calculation::MarketDollars.new(global_demand.airport, @date).business
       end
     end
 
-    def directional_government_dollars(global_demand, destination)
+    def directional_government_dollars(global_demand, route_demand)
       if global_demand.government == 0
         0
       else
-        RouteDemand.calculate(@date, global_demand.airport, destination).government.to_f / global_demand.government * Calculation::MarketDollars.new(global_demand.airport, @date).government
+        route_demand.government.to_f / global_demand.government * Calculation::MarketDollars.new(global_demand.airport, @date).government
       end
     end
 
-    def directional_leisure_dollars(global_demand, destination)
+    def directional_leisure_dollars(global_demand, route_demand)
       if global_demand.leisure == 0
         0
       else
-        RouteDemand.calculate(@date, global_demand.airport, destination).leisure.to_f / global_demand.leisure * Calculation::MarketDollars.new(global_demand.airport, @date).leisure
+        route_demand.leisure.to_f / global_demand.leisure * Calculation::MarketDollars.new(global_demand.airport, @date).leisure
       end
     end
 
-    def directional_tourist_dollars(global_demand, destination)
+    def directional_tourist_dollars(global_demand, route_demand)
       if global_demand.tourist == 0
         0
       else
-        RouteDemand.calculate(@date, global_demand.airport, destination).tourist.to_f / global_demand.tourist * Calculation::MarketDollars.new(global_demand.airport, @date).tourist
+        route_demand.tourist.to_f / global_demand.tourist * Calculation::MarketDollars.new(global_demand.airport, @date).tourist
       end
     end
 
@@ -63,7 +63,15 @@ class Calculation::RouteDollars
       @origin_global_demand ||= GlobalDemand.calculate(@date, @origin_airport)
     end
 
+    def origin_route_demand
+      @origin_route_demand ||= RouteDemand.calculate(@date, origin_global_demand.airport, @destination_airport)
+    end
+
     def destination_global_demand
       @destination_global_demand ||= GlobalDemand.calculate(@date, @destination_airport)
+    end
+
+    def destination_route_demand
+      @destination_route_demand ||= RouteDemand.calculate(@date, destination_global_demand.airport, @origin_airport)
     end
 end
