e105e4c7f26535ca9a1dfea869c12af910edb2f8
JBR <john.russell@tufts.edu>
2022-02-15 14:36:56 -0500

    RivalCountryGroup.rivals? returns false without database query when the arguments match

--- app/models/rival_country_group.rb
+++ app/models/rival_country_group.rb
@@ -5,8 +5,12 @@ class RivalCountryGroup < ApplicationRecord
   validate :countries_alphabetized
 
   def self.rivals?(country_group_1, country_group_2)
-    find_by(country_one: country_group_1, country_two: country_group_2).present? ||
-      find_by(country_one: country_group_2, country_two: country_group_1).present?
+    if country_group_1 == country_group_2
+      false
+    else
+      find_by(country_one: country_group_1, country_two: country_group_2).present? ||
+        find_by(country_one: country_group_2, country_two: country_group_1).present?
+    end
   end
 
   private
