802c7972cb39a81376ee4a3ac4648af2b6b1618f
JBR <john.russell@tufts.edu>
2022-02-25 17:01:36 -0500

    Calculation::InertiaRouteService

---
+++ app/models/calculation/inertia_route_service.rb
@@ -0,0 +1,175 @@
+class Calculation::InertiaRouteService
+  include Demandable
+
+  INERTIA_PLANE_FUEL_BURN_CONSTANT = 118.4
+  INERTIA_PLANE_MAX_NARROWBODY_SEATS = 199
+  INERTIA_PLANE_SPEED = 556
+  LOAD_FACTOR = 0.7
+  MAX_PASSENGERS_PER_FA = 50
+  REVENUE_PERCENTAGE = 0.5
+  LONG_DISTANCE = 4000
+  LONG_DISTANCE_BUSINESS_SEATS = 30
+  LONG_DISTANCE_PREMIUM_ECONOMY_SEATS = 50
+  LONG_DISTANCE_ECONOMY_SEATS = 225
+  SHORT_DISTANCE = 100
+  SHORT_DISTANCE_BUSINESS_SEATS = 0
+  SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS = 8
+  SHORT_DISTANCE_ECONOMY_SEATS = 57
+
+  def business_fare
+    if business_frequencies == 0
+      0
+    else
+      (business_frequencies / desired_business_frequencies) * (business_revenue / desired_business_frequencies / business_seats_per_flight)
+    end
+  end
+
+  def business_frequencies
+    if business_seats_per_flight == 0
+      0
+    else
+      (desired_business_frequencies / LOAD_FACTOR).ceil()
+    end
+  end
+
+  def business_seats_per_flight
+    if distance >= LONG_DISTANCE
+      LONG_DISTANCE_BUSINESS_SEATS
+    elsif distance >= SHORT_DISTANCE
+      SHORT_DISTANCE_BUSINESS_SEATS + ((LONG_DISTANCE_BUSINESS_SEATS - SHORT_DISTANCE_BUSINESS_SEATS) * (distance - SHORT_DISTANCE) / (LONG_DISTANCE - SHORT_DISTANCE)).ceil()
+    else
+      (SHORT_DISTANCE_BUSINESS_SEATS * distance / SHORT_DISTANCE).ceil()
+    end
+  end
+
+  def economy_fare
+    if economy_frequencies == 0
+      0
+    else
+      (economy_frequencies / desired_economy_frequencies) * (economy_revenue / desired_economy_frequencies / economy_seats_per_flight)
+    end
+  end
+
+  def economy_frequencies
+    if economy_seats_per_flight == 0
+      0
+    else
+      (desired_economy_frequencies / LOAD_FACTOR).ceil()
+    end
+  end
+
+  def economy_seats_per_flight
+    if distance >= LONG_DISTANCE
+      LONG_DISTANCE_ECONOMY_SEATS
+    elsif distance >= SHORT_DISTANCE
+      SHORT_DISTANCE_ECONOMY_SEATS + ((LONG_DISTANCE_ECONOMY_SEATS - SHORT_DISTANCE_ECONOMY_SEATS) * (distance - SHORT_DISTANCE) / (LONG_DISTANCE - SHORT_DISTANCE)).ceil()
+    else
+      (SHORT_DISTANCE_ECONOMY_SEATS * distance / SHORT_DISTANCE).ceil()
+    end
+  end
+
+  def premium_economy_fare
+    if premium_economy_frequencies == 0
+      0
+    else
+      (premium_economy_frequencies / desired_premium_economy_frequencies) * (premium_economy_revenue / desired_premium_economy_frequencies / premium_economy_seats_per_flight)
+    end
+  end
+
+  def premium_economy_frequencies
+    if premium_economy_seats_per_flight == 0
+      0
+    else
+      (desired_premium_economy_frequencies / LOAD_FACTOR).ceil()
+    end
+  end
+
+  def premium_economy_seats_per_flight
+    if distance >= LONG_DISTANCE
+      LONG_DISTANCE_PREMIUM_ECONOMY_SEATS
+    elsif distance >= SHORT_DISTANCE
+      SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS + ((LONG_DISTANCE_PREMIUM_ECONOMY_SEATS - SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS) * (distance - SHORT_DISTANCE) / (LONG_DISTANCE - SHORT_DISTANCE)).ceil()
+    else
+      (SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS * distance / SHORT_DISTANCE).ceil()
+    end
+  end
+
+  private
+
+    def business_flight_cost
+      flight_cost.to_f * business_seats_per_flight * Airplane::BUSINESS_SEAT_SIZE / total_seat_area
+    end
+
+    def business_revenue
+      revenue.max_business_class_revenue_per_week * REVENUE_PERCENTAGE
+    end
+
+    def desired_business_frequencies
+      business_revenue.to_f / business_flight_cost
+    end
+
+    def desired_economy_frequencies
+      economy_revenue.to_f / economy_flight_cost
+    end
+
+    def desired_premium_economy_frequencies
+      premium_economy_revenue.to_f / premium_economy_flight_cost
+    end
+
+    def economy_flight_cost
+      flight_cost.to_f * economy_seats_per_flight * Airplane::ECONOMY_SEAT_SIZE / total_seat_area
+    end
+
+    def economy_revenue
+      revenue.max_economy_class_revenue_per_week * REVENUE_PERCENTAGE
+    end
+
+    def flight_cost
+      @flight_cost ||= Calculation::FlightCostCalculator.new(inertia_airplane, distance).cost
+    end
+
+    def inertia_airplane
+      Airplane.new(
+        base_country_group: @origin.market.country_group,
+        business_seats: business_seats_per_flight,
+        construction_date: Date.today,
+        economy_seats: economy_seats_per_flight,
+        end_of_useful_life: Date.today + 1.day,
+        premium_economy_seats: premium_economy_seats_per_flight,
+        aircraft_model: AircraftModel.new(
+          name: "foo",
+          production_start_year: Date.today.year,
+          floor_space: business_seats_per_flight * Airplane::BUSINESS_SEAT_SIZE + premium_economy_seats_per_flight * Airplane::PREMIUM_ECONOMY_SEAT_SIZE + economy_seats_per_flight * Airplane::ECONOMY_SEAT_SIZE,
+          max_range: distance.ceil(),
+          fuel_burn: (Math.sqrt(total_seats) * INERTIA_PLANE_FUEL_BURN_CONSTANT).ceil(),
+          speed: INERTIA_PLANE_SPEED,
+          num_aisles: total_seats > INERTIA_PLANE_MAX_NARROWBODY_SEATS ? 2 : 1,
+          num_pilots: 2,
+          num_flight_attendants: total_seats <= 19 ? 0 : (total_seats / MAX_PASSENGERS_PER_FA.to_f).ceil(),
+          price: 1,
+          takeoff_distance: 1,
+          useful_life: 1,
+        )
+      )
+    end
+
+    def premium_economy_flight_cost
+      flight_cost.to_f * premium_economy_seats_per_flight * Airplane::PREMIUM_ECONOMY_SEAT_SIZE / total_seat_area
+    end
+
+    def premium_economy_revenue
+      revenue.max_premium_economy_class_revenue_per_week * REVENUE_PERCENTAGE
+    end
+
+    def revenue
+      @revenue ||= Calculation::MaximumRevenuePotential.new(@origin, @destination, @current_date)
+    end
+
+    def total_seat_area
+      economy_seats_per_flight * Airplane::ECONOMY_SEAT_SIZE + premium_economy_seats_per_flight * Airplane::PREMIUM_ECONOMY_SEAT_SIZE + business_seats_per_flight * Airplane::BUSINESS_SEAT_SIZE
+    end
+
+    def total_seats
+      economy_seats_per_flight + premium_economy_seats_per_flight + business_seats_per_flight
+    end
+end
---
+++ spec/models/calculation/inertia_route_service_spec.rb
@@ -0,0 +1,218 @@
+require "rails_helper"
+
+RSpec.describe Calculation::InertiaRouteService do
+  context "frequencies and fares that evaluate to integers" do
+    let(:origin) { instance_double(Airport, market: instance_double(Market, country_group: "Foo")) }
+    let(:destination) { instance_double(Airport) }
+    let(:date) { Date.today }
+    let(:flight_cost) { 17442 }
+    let(:flight_cost_calculator) { instance_double(Calculation::FlightCostCalculator, cost: flight_cost) }
+    let(:business_revenue) { 36720 }
+    let(:economy_revenue) { 107100 }
+    let(:premium_economy_revenue) { 30600 }
+    let(:revenue) {
+      instance_double(
+        Calculation::MaximumRevenuePotential,
+        max_business_class_revenue_per_week: business_revenue,
+        max_economy_class_revenue_per_week: economy_revenue,
+        max_premium_economy_class_revenue_per_week: premium_economy_revenue,
+      )
+    }
+    let(:distance) { Calculation::InertiaRouteService::LONG_DISTANCE }
+    let(:subject) { Calculation::InertiaRouteService.new(origin, destination, date) }
+
+    before(:each) do
+      allow(Calculation::MaximumRevenuePotential).to receive(:new).with(origin, destination, date).and_return(revenue)
+      allow(Calculation::FlightCostCalculator).to receive(:new).and_return(flight_cost_calculator)
+      allow(Calculation::Distance).to receive(:between_airports).and_return(distance)
+    end
+
+    it "calculates frequencies correctly" do
+      expect(subject.send(:desired_business_frequencies)).to eq 5
+      expect(subject.business_frequencies).to eq 8
+
+      expect(subject.send(:desired_economy_frequencies)).to eq 5
+      expect(subject.economy_frequencies).to eq 8
+
+      expect(subject.send(:desired_premium_economy_frequencies)).to eq 5
+      expect(subject.premium_economy_frequencies).to eq 8
+    end
+
+    it "calculates fares correctly" do
+      expect(subject.send(:business_revenue)).to eq business_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      assert_in_epsilon subject.business_fare, subject.send(:business_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS / 5, 0.000001
+
+      expect(subject.send(:business_flight_cost)).to be <= subject.business_fare * subject.business_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+
+      expect(subject.send(:economy_revenue)).to eq economy_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      assert_in_epsilon subject.economy_fare, subject.send(:economy_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS / 5, 0.000001
+
+      expect(subject.send(:economy_flight_cost)).to be <= subject.economy_fare * subject.economy_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+
+      expect(subject.send(:premium_economy_revenue)).to eq premium_economy_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      assert_in_epsilon subject.premium_economy_fare, subject.send(:premium_economy_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS / 5, 0.000001
+
+      expect(subject.send(:premium_economy_flight_cost)).to be <= subject.premium_economy_fare * subject.premium_economy_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+    end
+  end
+
+  context "frequencies and fares that do not evaluate to integers" do
+    let(:origin) { instance_double(Airport, market: instance_double(Market, country_group: "Foo")) }
+    let(:destination) { instance_double(Airport) }
+    let(:date) { Date.today }
+    let(:flight_cost) { 17442 }
+    let(:flight_cost_calculator) { instance_double(Calculation::FlightCostCalculator, cost: flight_cost) }
+    let(:business_revenue) { 36700 }
+    let(:economy_revenue) { 107000 }
+    let(:premium_economy_revenue) { 30000 }
+    let(:revenue) {
+      instance_double(
+        Calculation::MaximumRevenuePotential,
+        max_business_class_revenue_per_week: business_revenue,
+        max_economy_class_revenue_per_week: economy_revenue,
+        max_premium_economy_class_revenue_per_week: premium_economy_revenue,
+      )
+    }
+    let(:distance) { Calculation::InertiaRouteService::LONG_DISTANCE }
+    let(:subject) { Calculation::InertiaRouteService.new(origin, destination, date) }
+
+    before(:each) do
+      allow(Calculation::MaximumRevenuePotential).to receive(:new).with(origin, destination, date).and_return(revenue)
+      allow(Calculation::FlightCostCalculator).to receive(:new).and_return(flight_cost_calculator)
+      allow(Calculation::Distance).to receive(:between_airports).and_return(distance)
+    end
+
+    it "calculates frequencies correctly" do
+      expect(subject.send(:desired_business_frequencies)).to be > 4
+      expect(subject.send(:desired_business_frequencies)).to be < 5
+      expect(subject.business_frequencies).to eq 8
+
+      expect(subject.send(:desired_economy_frequencies)).to be > 4
+      expect(subject.send(:desired_economy_frequencies)).to be < 5
+      expect(subject.economy_frequencies).to eq 8
+
+      expect(subject.send(:desired_premium_economy_frequencies)).to be > 4
+      expect(subject.send(:desired_premium_economy_frequencies)).to be < 5
+      expect(subject.premium_economy_frequencies).to eq 8
+    end
+
+    it "calculates fares correctly" do
+      expect(subject.send(:business_revenue)).to eq business_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      expect(subject.business_fare).to be > subject.send(:business_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS / 5
+      expect(subject.business_fare).to be < subject.send(:business_revenue) * (8/4.0) / Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS / 4
+
+      expect(subject.send(:business_flight_cost)).to be <= subject.business_fare * subject.business_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+
+      expect(subject.send(:economy_revenue)).to eq economy_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      expect(subject.economy_fare).to be > subject.send(:economy_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS / 5
+      expect(subject.economy_fare).to be < subject.send(:economy_revenue) * (8/4.0) / Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS / 4
+
+      expect(subject.send(:economy_flight_cost)).to be <= subject.economy_fare * subject.economy_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+
+      expect(subject.send(:premium_economy_revenue)).to eq premium_economy_revenue * Calculation::InertiaRouteService::REVENUE_PERCENTAGE
+      expect(subject.premium_economy_fare).to be > subject.send(:premium_economy_revenue) * (8/5.0) / Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS / 5
+      expect(subject.premium_economy_fare).to be < subject.send(:premium_economy_revenue) * (8/4.0) / Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS / 4
+
+      expect(subject.send(:premium_economy_flight_cost)).to be <= subject.premium_economy_fare * subject.premium_economy_seats_per_flight * Calculation::InertiaRouteService::LOAD_FACTOR
+    end
+  end
+
+  context "business_seats_per_flight" do
+    subject = Calculation::InertiaRouteService.new(Airport.new, Airport.new, Date.today)
+
+    it "is less than SHORT_DISTANCE_BUSINESS_SEATS for a flight of less than SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE / 2)
+
+      assert subject.business_seats_per_flight == 0 || subject.business_seats_per_flight < Calculation::InertiaRouteService::SHORT_DISTANCE_BUSINESS_SEATS
+    end
+
+    it "is SHORT_DISTANCE_BUSINESS_SEATS for a flight of SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE)
+
+      assert subject.business_seats_per_flight == Calculation::InertiaRouteService::SHORT_DISTANCE_BUSINESS_SEATS
+    end
+
+    it "is between SHORT_DISTANCE_BUSINESS_SEATS and LONG_DISTANCE_BUSINESS_SEATS for a flight between SHORT_DISTANCE and LONG_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return((Calculation::InertiaRouteService::SHORT_DISTANCE + Calculation::InertiaRouteService::LONG_DISTANCE) / 2)
+
+      assert subject.business_seats_per_flight > Calculation::InertiaRouteService::SHORT_DISTANCE_BUSINESS_SEATS
+      assert subject.business_seats_per_flight < Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS
+    end
+
+    it "is LONG_DISTANCE_BUSINESS_SEATS for a flight of LONG_DISTANCE or more" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE)
+
+      assert subject.business_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS
+
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE * 2)
+
+      assert subject.business_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_BUSINESS_SEATS
+    end
+  end
+
+  context "economy_seats_per_flight" do
+    subject = Calculation::InertiaRouteService.new(Airport.new, Airport.new, Date.today)
+
+    it "is less than SHORT_DISTANCE_ECONOMY_SEATS for a flight of less than SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE / 2)
+
+      assert subject.economy_seats_per_flight == 0 || subject.economy_seats_per_flight < Calculation::InertiaRouteService::SHORT_DISTANCE_ECONOMY_SEATS
+    end
+
+    it "is SHORT_DISTANCE_ECONOMY_SEATS for a flight of SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE)
+
+      assert subject.economy_seats_per_flight == Calculation::InertiaRouteService::SHORT_DISTANCE_ECONOMY_SEATS
+    end
+
+    it "is between SHORT_DISTANCE_ECONOMY_SEATS and LONG_DISTANCE_ECONOMY_SEATS for a flight between SHORT_DISTANCE and LONG_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return((Calculation::InertiaRouteService::SHORT_DISTANCE + Calculation::InertiaRouteService::LONG_DISTANCE) / 2)
+
+      assert subject.economy_seats_per_flight > Calculation::InertiaRouteService::SHORT_DISTANCE_ECONOMY_SEATS
+      assert subject.economy_seats_per_flight < Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS
+    end
+
+    it "is LONG_DISTANCE_ECONOMY_SEATS for a flight of LONG_DISTANCE or more" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE)
+
+      assert subject.economy_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS
+
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE * 2)
+
+      assert subject.economy_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_ECONOMY_SEATS
+    end
+  end
+
+  context "premium_economy_seats_per_flight" do
+    subject = Calculation::InertiaRouteService.new(Airport.new, Airport.new, Date.today)
+
+    it "is less than SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS for a flight of less than SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE / 2)
+
+      assert subject.premium_economy_seats_per_flight == 0 || subject.premium_economy_seats_per_flight < Calculation::InertiaRouteService::SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS
+    end
+
+    it "is SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS for a flight of SHORT_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::SHORT_DISTANCE)
+
+      assert subject.premium_economy_seats_per_flight == Calculation::InertiaRouteService::SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS
+    end
+
+    it "is between SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS and LONG_DISTANCE_PREMIUM_ECONOMY_SEATS for a flight between SHORT_DISTANCE and LONG_DISTANCE" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return((Calculation::InertiaRouteService::SHORT_DISTANCE + Calculation::InertiaRouteService::LONG_DISTANCE) / 2)
+
+      assert subject.premium_economy_seats_per_flight > Calculation::InertiaRouteService::SHORT_DISTANCE_PREMIUM_ECONOMY_SEATS
+      assert subject.premium_economy_seats_per_flight < Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS
+    end
+
+    it "is LONG_DISTANCE_PREMIUM_ECONOMY_SEATS for a flight of LONG_DISTANCE or more" do
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE)
+
+      assert subject.premium_economy_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS
+
+      allow(Calculation::Distance).to receive(:between_airports).and_return(Calculation::InertiaRouteService::LONG_DISTANCE * 2)
+
+      assert subject.premium_economy_seats_per_flight == Calculation::InertiaRouteService::LONG_DISTANCE_PREMIUM_ECONOMY_SEATS
+    end
+  end
+end
