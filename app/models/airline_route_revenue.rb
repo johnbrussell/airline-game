3261dc7e94ba6e95dbe4ae723c7433ceaa32b206
JBR <john.russell@tufts.edu>
2022-02-26 18:41:49 -0500

    Validate the number of passengers assigned to an airline on a route does not exceed the number of seats the airline flies on the route

--- app/models/airline_route_revenue.rb
+++ app/models/airline_route_revenue.rb
@@ -9,10 +9,17 @@ class AirlineRouteRevenue < ApplicationRecord
   validates :revenue, presence: true
   validates :revenue, numericality: { greater_than_or_equal_to: 0 }
   validate :revenue_calculated_correctly
+  validate :enough_seating_for_passengers
 
   belongs_to :airline_route
 
   private
+
+    def enough_seating_for_passengers
+      if airline_route.total_economy_seats < economy_pax || airline_route.total_premium_economy_seats < premium_economy_pax || airline_route.total_business_seats < business_pax
+        errors.add(:seats, "not sufficient to seat passengers")
+      end
+    end
 
     def revenue_calculated_correctly
       pax_revenue = airline_route.economy_price * economy_pax + airline_route.premium_economy_price * premium_economy_pax + airline_route.business_price * business_pax
