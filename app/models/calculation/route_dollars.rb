d16f20f653129674945498fc78e522e1d6c96c7d
JBR <john.russell@tufts.edu>
2022-03-09 20:27:57 -0500

    Exclusive route dollars

--- app/models/calculation/route_dollars.rb
+++ app/models/calculation/route_dollars.rb
@@ -10,6 +10,26 @@ class Calculation::RouteDollars
       directional_business_dollars(destination_global_demand, origin_route_demand)
   end
 
+  def exclusive_business
+    directional_exclusive_business_dollars(origin_global_demand, destination_route_demand) +
+      directional_exclusive_business_dollars(destination_global_demand, origin_route_demand)
+  end
+
+  def exclusive_government
+    directional_exclusive_government_dollars(origin_global_demand, destination_route_demand) +
+      directional_exclusive_government_dollars(destination_global_demand, origin_route_demand)
+  end
+
+  def exclusive_leisure
+    directional_exclusive_leisure_dollars(origin_global_demand, destination_route_demand) +
+      directional_exclusive_leisure_dollars(destination_global_demand, origin_route_demand)
+  end
+
+  def exclusive_tourist
+    directional_exclusive_tourist_dollars(origin_global_demand, destination_route_demand) +
+      directional_exclusive_tourist_dollars(destination_global_demand, origin_route_demand)
+  end
+
   def government
     directional_government_dollars(origin_global_demand, destination_route_demand) +
       directional_government_dollars(destination_global_demand, origin_route_demand)
@@ -32,6 +52,38 @@ class Calculation::RouteDollars
         0
       else
         route_demand.business.to_f / global_demand.business * Calculation::MarketDollars.new(global_demand.airport, @date).business
+      end
+    end
+
+    def directional_exclusive_business_dollars(global_demand, route_demand)
+      if global_demand.business == 0
+        0
+      else
+        route_demand.exclusive_business.to_f / global_demand.business * Calculation::MarketDollars.new(global_demand.airport, @date).business
+      end
+    end
+
+    def directional_exclusive_government_dollars(global_demand, route_demand)
+      if global_demand.government == 0
+        0
+      else
+        route_demand.exclusive_government.to_f / global_demand.government * Calculation::MarketDollars.new(global_demand.airport, @date).government
+      end
+    end
+
+    def directional_exclusive_leisure_dollars(global_demand, route_demand)
+      if global_demand.leisure == 0
+        0
+      else
+        route_demand.exclusive_leisure.to_f / global_demand.leisure * Calculation::MarketDollars.new(global_demand.airport, @date).leisure
+      end
+    end
+
+    def directional_exclusive_tourist_dollars(global_demand, route_demand)
+      if global_demand.tourist == 0
+        0
+      else
+        route_demand.exclusive_tourist.to_f / global_demand.tourist * Calculation::MarketDollars.new(global_demand.airport, @date).tourist
       end
     end
 
