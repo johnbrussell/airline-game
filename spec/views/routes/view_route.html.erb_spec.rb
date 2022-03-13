24527f5af87a4bef61e352bf0df6b16c5edcbf4d
JBR <john.russell@tufts.edu>
2022-03-12 21:35:10 -0500

    Link to view all user airline routes from routes page

--- spec/views/routes/view_route.html.erb_spec.rb
+++ spec/views/routes/view_route.html.erb_spec.rb
@@ -25,6 +25,18 @@ RSpec.describe "routes/view_route", type: :feature do
     expect(page).to have_content "Airline Game Home"
   end
 
+  it "has a link to view all airline routes" do
+    game = Fabricate(:game)
+    Fabricate(:airline, is_user_airline: true, game_id: game.id, base_id: Market.last.id)
+    visit game_airline_route_add_flights_path(game, -1, params: { origin_id: Airport.find_by(iata: "FUN"), destination_id: Airport.find_by(iata: "INU")})
+
+    expect(page).to have_content "FUN - INU"
+
+    click_link "View #{game.user_airline.name} routes"
+
+    expect(page).to have_content "#{game.user_airline.name} routes"
+  end
+
   it "has a link to view a different route that remembers which route it was on" do
     game = Fabricate(:game)
     Fabricate(:airline, is_user_airline: true, game_id: game.id, name: "TIA", base_id: Market.last.id)
