49b4d84889967889cba386ff7b348d39c983d9ec
JBR <john.russell@tufts.edu>
2022-03-24 21:55:00 -0400

    Calculation::Distance.market_distance

--- test/models/calculation/distance_test.rb
+++ test/models/calculation/distance_test.rb
@@ -4,21 +4,28 @@ class Calculation::DistanceTest < ActiveSupport::TestCase
   airport1 = Airport.new(latitude: -0.547458, longitude: 166.919006)
   airport2 = Airport.new(latitude: 21.317817, longitude: -157.920227)
   airport3 = Airport.new(latitude: 51.4706, longitude: -0.461941)
+  market1 = Market.new(latitude: -0.547458, longitude: 166.919006)
+  market2 = Market.new(latitude: 21.317817, longitude: -157.920227)
+  market3 = Market.new(latitude: 51.4706, longitude: -0.461941)
 
   test "test equivalent points yield zero distance" do
     assert Calculation::Distance.between_airports(airport1, airport1) == 0
     assert Calculation::Distance.between_airports(airport2, airport2) == 0
+    assert Calculation::Distance.between_markets(market1, market1) == 0
+    assert Calculation::Distance.between_markets(market2, market2) == 0
   end
 
   test "test a moderate example" do
     expected = 2812
 
     assert (Calculation::Distance.between_airports(airport1, airport2) - expected).abs < 1
+    assert (Calculation::Distance.between_markets(market1, market2) - expected).abs < 1
   end
 
   test "test a long example" do
     expected = 8843
 
     assert (Calculation::Distance.between_airports(airport1, airport3) - expected).abs < 1
+    assert (Calculation::Distance.between_markets(market1, market3) - expected).abs < 1
   end
 end
