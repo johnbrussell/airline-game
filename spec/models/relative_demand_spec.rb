324313e732962a1e3cc33653239b504b09df1936
JBR <john.russell@tufts.edu>
2022-10-08 22:22:57 -0400

    Test nil origin_airport and destination_airport on RelativeDemand

--- spec/models/relative_demand_spec.rb
+++ spec/models/relative_demand_spec.rb
@@ -42,5 +42,21 @@ RSpec.describe RelativeDemand do
       RelativeDemand.create!(origin_market_id: market_3.id, destination_market_id: market_2.id, destination_airport_iata: "FUN", **shared_inputs)
       expect(RelativeDemand.new(origin_market_id: market_1.id, destination_market_id: market_2.id, destination_airport_iata: "FUN", **shared_inputs).save).to be false
     end
+
+    it "has nil origin_airport and destination_airport when they are not specified" do
+      shared_inputs = {
+        business: 1,
+        government: 1,
+        leisure: 1,
+        tourist: 1,
+        pct_economy: 1,
+        pct_premium_economy: 1,
+        pct_business: 1,
+        last_measured: Date.today,
+      }
+      subject = RelativeDemand.create!(origin_market_id: market_1.id, destination_market_id: market_2.id, **shared_inputs)
+      expect(subject.origin_airport).to be nil
+      expect(subject.destination_airport).to be nil
+    end
   end
 end
