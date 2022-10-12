167b827b2d5dbdffc1e5b424768659f30eb1433d
John Russell <john.russell@tufts.edu>
2022-10-11 21:23:40 -0400

    Clean up RelativeDemand (#211)
    * Allow Calculation::RelativeDemand to be passed nil airports
    
    * Move calculation of RelativeDemand into the RelativeDemand class

--- app/models/relative_demand.rb
+++ app/models/relative_demand.rb
@@ -20,4 +20,26 @@ class RelativeDemand < ApplicationRecord
   belongs_to :destination_market, class_name: "Market"
   belongs_to :origin_airport, class_name: "Airport", foreign_key: :origin_airport_iata, primary_key: :iata, optional: true
   belongs_to :destination_airport, class_name: "Airport", foreign_key: :destination_airport_iata, primary_key: :iata, optional: true
+
+  def self.calculate(date, origin_airport, destination_airport, origin_market, destination_market)
+    calculator = Calculation::RelativeDemand.new(date, origin_airport, destination_airport, origin_market, destination_market)
+    RelativeDemand.find_or_initialize_by(
+      origin_market_id: origin_market.id,
+      destination_market_id: destination_market.id,
+      origin_airport_iata: origin_airport ? origin_airport.iata : "",
+      destination_airport_iata: destination_airport ? destination_airport.iata : "",
+      last_measured: date,
+    ).tap do |relative_demand|
+      relative_demand.assign_attributes(
+        business: calculator.business,
+        government: calculator.government,
+        leisure: calculator.leisure,
+        tourist: calculator.tourist,
+        pct_business: 1 / 3.0,
+        pct_economy: 1 / 3.0,
+        pct_premium_economy: 1 / 3.0,
+      )
+      relative_demand.save!
+    end
+  end
 end
