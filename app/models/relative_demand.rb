1b6c93d5f706050f975c091da316f928c6023daa
JBR <john.russell@tufts.edu>
2022-10-17 18:06:50 -0400

    Return extant RelativeDemands when they exist

--- app/models/relative_demand.rb
+++ app/models/relative_demand.rb
@@ -22,15 +22,21 @@ class RelativeDemand < ApplicationRecord
   belongs_to :destination_airport, class_name: "Airport", foreign_key: :destination_airport_iata, primary_key: :iata, optional: true
 
   def self.calculate(date, origin_airport, destination_airport, origin_market, destination_market)
-    calculator = Calculation::RelativeDemand.new(date, origin_airport, destination_airport, origin_market, destination_market)
-    RelativeDemand.find_or_initialize_by(
+    relative_demand = RelativeDemand.find_by(
       origin_market_id: origin_market.id,
       destination_market_id: destination_market.id,
       origin_airport_iata: origin_airport ? origin_airport.iata : "",
       destination_airport_iata: destination_airport ? destination_airport.iata : "",
       last_measured: date,
-    ).tap do |relative_demand|
-      relative_demand.assign_attributes(
+    )
+    if relative_demand.nil?
+      calculator = Calculation::RelativeDemand.new(date, origin_airport, destination_airport, origin_market, destination_market)
+      RelativeDemand.create!(
+        origin_market_id: origin_market.id,
+        destination_market_id: destination_market.id,
+        origin_airport_iata: origin_airport ? origin_airport.iata : "",
+        destination_airport_iata: destination_airport ? destination_airport.iata : "",
+        last_measured: date,
         business: calculator.business,
         government: calculator.government,
         leisure: calculator.leisure,
@@ -39,7 +45,8 @@ class RelativeDemand < ApplicationRecord
         pct_economy: 1 / 3.0,
         pct_premium_economy: 1 / 3.0,
       )
-      relative_demand.save!
+    else
+      relative_demand
     end
   end
 end
