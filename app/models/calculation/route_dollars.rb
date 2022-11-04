d17231fec33df2f8eb22a15adef81ce1b3158140
JBR <john.russell@tufts.edu>
2022-11-03 20:53:28 -0400

    Calculation::RouteDollars.business_class_dollars, economy_class_dollars, and premium_economy_class_dollars

--- app/models/calculation/route_dollars.rb
+++ app/models/calculation/route_dollars.rb
@@ -1,6 +1,8 @@
 class Calculation::RouteDollars
   def initialize(date, origin_market, destination_market, origin_airport, destination_airport)
     @date = date
+    @origin_market = origin_market
+    @destination_market = destination_market
     @origin_airport = origin_airport
     @destination_airport = destination_airport
   end
@@ -10,9 +12,21 @@ class Calculation::RouteDollars
       directional_business_dollars(destination_global_demand, origin_route_demand)
   end
 
+  def business_class_dollars
+    business_dollars * class_calculator.pct_business_dollars_business + leisure_dollars * class_calculator.pct_leisure_dollars_business
+  end
+
+  def economy_class_dollars
+    business_dollars * class_calculator.pct_business_dollars_economy + leisure_dollars * class_calculator.pct_leisure_dollars_economy
+  end
+
   def exclusive_business
     directional_exclusive_business_dollars(origin_global_demand, destination_route_demand) +
       directional_exclusive_business_dollars(destination_global_demand, origin_route_demand)
+  end
+
+  def premium_economy_class_dollars
+    business_dollars * class_calculator.pct_business_dollars_premium_economy + leisure_dollars * class_calculator.pct_leisure_dollars_premium_economy
   end
 
   def exclusive_government
@@ -46,6 +60,34 @@ class Calculation::RouteDollars
   end
 
   private
+
+    def business_dollars
+      business_dollars_from_business + business_dollars_from_government
+    end
+
+    def business_dollars_from_business
+      market_dollars.business * relative_demand.business / total_market_demand.business
+    end
+
+    def business_dollars_from_government
+      market_dollars.government * relative_demand.government / total_market_demand.government
+    end
+
+    def class_calculator
+      @class_calculator ||= Calculation::ClassOfService.new(relative_demand.distance)
+    end
+
+    def leisure_dollars
+      leisure_dollars_from_leisure + leisure_dollars_from_tourist
+    end
+
+    def leisure_dollars_from_leisure
+      market_dollars.leisure * relative_demand.leisure / total_market_demand.leisure
+    end
+
+    def leisure_dollars_from_tourist
+      market_dollars.tourist * relative_demand.tourist / total_market_demand.tourist
+    end
 
     def directional_business_dollars(global_demand, route_demand)
       if global_demand.business == 0
@@ -125,5 +167,17 @@ class Calculation::RouteDollars
 
     def destination_route_demand
       @destination_route_demand ||= RouteDemand.calculate(@date, @origin_airport, @destination_airport)
+    end
+
+    def market_dollars
+      @market_dollars ||= MarketDollars.calculate(@origin_market, @date)
+    end
+
+    def relative_demand
+      @relative_demand ||= RelativeDemand.most_recent_or_initialize(@date, @origin_airport, @destination_airport, @origin_market, @destination_market)
+    end
+
+    def total_market_demand
+      @total_market_demand ||= TotalMarketDemand.calculate(@origin_market, @date)
     end
 end
--- spec/models/calculation/route_dollars_spec.rb
+++ spec/models/calculation/route_dollars_spec.rb
@@ -12,6 +12,26 @@ RSpec.describe Calculation::RouteDollars do
 
     market_dollars = instance_double(Calculation::MarketDollars, business: 5000, government: 10000, leisure: 50000, tourist: 1000)
     allow(Calculation::MarketDollars).to receive(:new).and_return(market_dollars)
+
+    class_calculator = instance_double(
+      Calculation::ClassOfService,
+      pct_business_dollars_business: 0.10,
+      pct_business_dollars_economy: 0.70,
+      pct_business_dollars_premium_economy: 0.20,
+      pct_leisure_dollars_business: 0.01,
+      pct_leisure_dollars_economy: 0.90,
+      pct_leisure_dollars_premium_economy: 0.09,
+    )
+    allow(Calculation::ClassOfService).to receive(:new).and_return(class_calculator)
+
+    market_dollars = instance_double(MarketDollars, business: 1000.0, government: 100.0, leisure: 10000.0, tourist: 1000.0)
+    allow(MarketDollars).to receive(:calculate).and_return market_dollars
+
+    relative_demand = instance_double(RelativeDemand, business: 1.0, government: 2.0, leisure: 3.0, tourist: 4.0, distance: 5)
+    allow(RelativeDemand).to receive(:most_recent_or_initialize).and_return relative_demand
+
+    total_market_demand = instance_double(TotalMarketDemand, business: 100.0, government: 100.0, leisure: 1000.0, tourist: 1000.0)
+    allow(TotalMarketDemand).to receive(:calculate).and_return total_market_demand
   end
 
   context "business" do
@@ -22,6 +42,47 @@ RSpec.describe Calculation::RouteDollars do
       subject = Calculation::RouteDollars.new(date, origin.market, destination.market, origin, destination)
 
       expect(subject.business).to eq 1000
+    end
+  end
+
+  context "business_class_dollars" do
+    let(:origin_market) { Fabricate(:market, name: "New Haven") }
+    let(:destination_market) { Fabricate(:market, name: "New York City") }
+    let(:origin_airport) { Fabricate(:airport, iata: "HVN", market: origin_market) }
+    let(:destination_airport) { Fabricate(:airport, iata: "LGA", market: destination_market) }
+
+    it "calculates correctly" do
+      subject = Calculation::RouteDollars.new(Date.today, origin_market, destination_market, origin_airport, destination_airport)
+
+      expect(subject.send(:business_dollars)).to eq 12
+      expect(subject.send(:leisure_dollars)).to eq 34
+      assert_in_epsilon subject.business_class_dollars, 1.54, 0.000000001
+    end
+  end
+
+  context "economy_class_dollars" do
+    let(:origin_market) { Fabricate(:market, name: "New Haven") }
+    let(:destination_market) { Fabricate(:market, name: "New York City") }
+    let(:origin_airport) { Fabricate(:airport, iata: "HVN", market: origin_market) }
+    let(:destination_airport) { Fabricate(:airport, iata: "LGA", market: destination_market) }
+
+    it "calculates correctly" do
+      subject = Calculation::RouteDollars.new(Date.today, origin_market, destination_market, origin_airport, destination_airport)
+
+      assert_in_epsilon subject.economy_class_dollars, 12 * 0.7 + 34 * 0.9, 0.000000001
+    end
+  end
+
+  context "premium_economy_class_dollars" do
+    let(:origin_market) { Fabricate(:market, name: "New Haven") }
+    let(:destination_market) { Fabricate(:market, name: "New York City") }
+    let(:origin_airport) { Fabricate(:airport, iata: "HVN", market: origin_market) }
+    let(:destination_airport) { Fabricate(:airport, iata: "LGA", market: destination_market) }
+
+    it "calculates correctly" do
+      subject = Calculation::RouteDollars.new(Date.today, origin_market, destination_market, origin_airport, destination_airport)
+
+      assert_in_epsilon subject.premium_economy_class_dollars, 12 * 0.2 + 34 * 0.09, 0.000000001
     end
   end
 
