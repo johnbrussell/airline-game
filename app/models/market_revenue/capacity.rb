31fc2c92ec57cd0d6fd69b5a0483f0fb84081dc4
JBR <john.russell@tufts.edu>
2022-11-23 12:23:16 -0500

    Data structures for revenue allocation

---
+++ app/models/market_revenue/capacity.rb
@@ -0,0 +1,1 @@
+MarketRevenue::Capacity = Struct.new(:airplane_route, :reputation_data, :available_seats, :origin_iata, :destination_iata, :reputation, :dollars_to_fill)
---
+++ app/models/market_revenue/route_service.rb
@@ -0,0 +1,1 @@
+MarketRevenue::RouteService = Struct.new(:reputation_data, :frequencies, :seats_per_flight, :origin_iata, :destination_iata)
